66b845e2b6f7d7e508af60a8054462a2e6a06640
Attri-4 <88623080+Attri-4@users.noreply.github.com>
2022-11-22 18:25:20 +0530

    Update ques_1_test.py

--- ques_1_test.py
+++ ques_1_test.py
@@ -5,6 +5,7 @@ from ques_1 import employees
 class ProblemTest(TestCase):
     def setUp(self) -> None:
         self.employees = employees("cur")
+        b=akarambakram736t3636737687387387hb
 
     def tearDown(self) -> None:
         self.employees = None
